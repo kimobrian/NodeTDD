9898102bb3426b15437bb5ac6a74cebe7310f057
kimobrian <brian.kimokoti@andela.com>
2017-05-15 22:21:51 +0300

    Node TDD

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "db":{
+        "testing":"mongodb://127.0.0.1:27017/node_test",
+        "develop":"mongodb://127.0.0.1:27017/node_dev",
+        "production":"mongodb://127.0.0.1:27017/node_prod"
+    },
+    "port":7500
+}
